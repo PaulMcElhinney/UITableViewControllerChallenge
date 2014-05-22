2c5d08e92f76076a66986a6c23a53df0f189ff52
Paul McElhinney <paulmcelhinney@Pauls-MacBook-Pro.local>
2014-05-22 13:35:24 -0400

    Initial Commit

---
+++ UITableViewController Challenge Solution/PGMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PGMAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Paul McElhinney on 5/22/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PGMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/PGMViewController.h
@@ -0,0 +1,13 @@
+//
+//  PGMViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Paul McElhinney on 5/22/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PGMViewController : UIViewController
+
+@end
